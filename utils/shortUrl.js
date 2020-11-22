25415fd312a92f0fffc7a6e945d04e15b2c7434d
Gabo.codes <gabedu2000@gmail.com>
2020-11-22 23:44:29 +0000

    shortURL Function

--- utils/shortUrl.js
+++ utils/shortUrl.js
@@ -1,5 +1,13 @@
-const shortUrl = async () => {
+const { API_SHORT_URL_KEY } = process.env;
 
+const shortUrl = async (longUrl) => {
+    const options = {
+        method: 'POST',
+        contentType: 'application/json',
+        body: JSON.stringify({ longUrl })
+      }
+    const response = await fetch(`http://cromona.co/shortUrl/?apiKey=${API_SHORT_URL_KEY}`, options);
+    return response.json();
 };
 
 module.exports = shortUrl;
