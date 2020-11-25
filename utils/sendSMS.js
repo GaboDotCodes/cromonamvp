31b013f86f5b346e5d5db7069c163742a9217103
GaboDotCodes <gabedu2000@gmail.com>
2020-11-25 15:55:25 -0500

    sendsms

--- utils/sendSMS.js
+++ utils/sendSMS.js
@@ -1,17 +1,19 @@
+const fetch = require("node-fetch");
+
 const { CLICKATELL_APIKEY } = process.env;
 
 const sendSMS = async (content, toNumber) => {
   const bodyRaw = { messages: [ { channel: 'sms', to: `+57${toNumber}`, content } ] };
   const options = {
     method: 'POST',
-    contentType: 'application/json',
     body: JSON.stringify(bodyRaw),
     headers: {
-        Authorization: CLICKATELL_APIKEY
+      'Content-Type': 'application/json',
+      authorization: CLICKATELL_APIKEY
     }
   };
   const response = await fetch('https://platform.clickatell.com/v1/message', options);
   return response.json();
 };
 
-module.exports = sendSMS;
+module.exports = { sendSMS };
