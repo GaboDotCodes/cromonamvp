06b42ef9927ac3dc084cb497bcfd20f6ef4268f7
GaboDotCodes <gabedu2000@gmail.com>
2020-12-13 14:14:20 -0500

    Change server domain

--- utils/shortUrl.js
+++ utils/shortUrl.js
@@ -10,9 +10,9 @@ const shortUrl = async (longUrl) => {
         },
         body: JSON.stringify({ longUrl })
       }
-    const response = await fetch(`https://cromona.co/shortUrl/?apiKey=${API_SHORT_URL_KEY}`, options);
+    const response = await fetch(`https://cromona.herokuapp.com/shortUrl/?apiKey=${API_SHORT_URL_KEY}`, options);
     const { shortUrl: id } = await response.json();
-    return `https://cromona.co/s/${id}`
+    return `https://cromona.herokuapp.com/s/${id}`
 };
 
 module.exports = { shortUrl };
